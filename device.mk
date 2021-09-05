18c8a6c2cad1845797c86573da72ba179984c95f
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-05 14:49:50 +0300

    pissarro: vibrator: Avoid target name conflicts
    Change-Id: Icd298d69b9719307cf62bd10c629906f878de04b

--- device.mk
+++ device.mk
@@ -329,6 +329,13 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
     android.hardware.usb.gadget@1.1.vendor
 
+# Vibrator
+PRODUCT_PACKAGES += \
+   vendor.qti.hardware.vibrator.service.pissarro
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service-lazy \
