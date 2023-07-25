ed369b8db3d83f7abb0b2325ba39df018d8862f3
bengris32 <bengris32@protonmail.ch>
2023-07-25 09:35:55 +0100

    pissarro: Add shim for proprietary AudioTrack constructor
    Change-Id: Icc7a1f9c748578f5b58499e7a76213673c4bcf54
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- extract-files.sh
+++ extract-files.sh
@@ -65,6 +65,9 @@ function blob_fixup {
         vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
             "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "$2"
             ;;
+        system/lib64/libsink.so)
+            "${PATCHELF}" --add-needed "libshim_sink.so" "$2"
+            ;;
     esac
 }
 
