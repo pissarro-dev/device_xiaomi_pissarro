4204b20fa03a02638f62d468a7941ddeda5dd019
bengris32 <bengris32@protonmail.ch>
2023-07-25 14:22:20 +0100

    pissarro: Import stock CarrierConfig overlay
    * Main carrier config from MtkCarrierConfig asset default_v2.9.3.xml,
      with vendor specific settings appended to the end.
    
    Change-Id: I7bcb5f9714720e3b42b5b1e5dc13cfe4233ac874
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -175,6 +175,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+   CarrierConfigOverlayPissarro \
    FrameworksResOverlayPissarro \
    SystemUIOverlayPissarro \
    TelephonyResOverlayPissarro \
