c422e71e4e3bdf008b95d4c9245eedd200c8665e
rio004 <riyon336@gmail.com>
2023-09-27 16:31:00 +0100

    pissarro: Specify recovery flags
    Signed-off-by: rio004 <riyon336@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,6 +92,9 @@ BOARD_HAS_MTK_HARDWARE := true
 # Recovery
 BOARD_USES_RECOVERY_AS_BOOT := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6877
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # Inherit the proprietary files
 include vendor/xiaomi/pissarro/BoardConfigVendor.mk
