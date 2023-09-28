bd17145a1540c67fdfdf485bc1b9ef5fa9f24212
rio004 <riyon336@gmail.com>
2023-09-28 10:43:29 +0100

    pissarro: Import Bluetooth stack
    * Reference: https://github.com/moto-common/android_device_mediatek_common/blob/master/vendor/bt/proprietary-files.txt
    
    Co-authored-by: electimon <electimon@gmail.com>

--- device.mk
+++ device.mk
@@ -47,6 +47,9 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.3-impl:32
 
 PRODUCT_PACKAGES += \
+    audio.bluetooth.default
+
+PRODUCT_PACKAGES += \
     libaudiofoundation.vendor \
     libalsautils \
     libtinycompress \
@@ -62,6 +65,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth@1.1.vendor \
+    libbluetooth_audio_session
 
 # Health
 PRODUCT_PACKAGES += \
