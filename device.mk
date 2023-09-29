4c2f02a505e0e091692db4c086c020eaf2fafdb0
rio004 <riyon336@gmail.com>
2023-09-29 20:30:26 +0100

    pissarro: Configure Thermal stack

--- device.mk
+++ device.mk
@@ -233,6 +233,11 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek \
     hardware/xiaomi
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@2.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
