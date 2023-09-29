fc4ddf4040ed57345ca8aca74535b00eaedcb44d
rio004 <riyon336@gmail.com>
2023-09-29 21:29:13 +0100

    pissarro: Import GNSS stack
    * Reference: https://github.com/moto-common/android_device_mediatek_common/tree/master/vendor/gnss
    
    Co-authored-by: electimon <electimon@gmail.com>
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: rio004 <riyon336@gmail.com>

--- device.mk
+++ device.mk
@@ -103,6 +103,13 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
+    android.hardware.gnss.visibility_control@1.0.vendor \
+    android.hardware.gnss-V1-ndk.vendor \
+    android.hardware.gnss@2.1.vendor
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
