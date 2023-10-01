57c6ea0f3dbb4843220b198409743a299b76f59a
rio004 <riyon336@gmail.com>
2023-10-01 23:40:21 +0100

    pissarro: Import DRM stack

--- device.mk
+++ device.mk
@@ -95,6 +95,10 @@ PRODUCT_PACKAGES += \
     libdrm.vendor \
     libutils-v32
 
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
