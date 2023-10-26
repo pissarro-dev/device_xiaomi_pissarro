90270fd7a09441a8d4d246199d7c9700e08b0197
LuK1337 <priv.luk@gmail.com>
2023-10-26 15:21:23 +0100

    pissarro: sepolicy: Label wakeup nodes
    for i in $(realpath /sys/class/wakeup/*); do if ! echo "$(ls -dZ $i)" | grep -q sysfs_wakeup; then echo genfscon sysfs $(echo $i | sed -e 's|/sys||g' -e 's|wakeup/wakeup.|wakeup|g' -e 's|wakeup[0-9]*|wakeup|g') u:object_r:sysfs_wakeup:s0; fi; done | sort -u
    
    Change-Id: Id70f4e725f8925b31c0007fae5eb98756ddcd410

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,6 +134,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2023-11-01
