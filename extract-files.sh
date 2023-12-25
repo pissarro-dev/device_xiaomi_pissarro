cb7c8cb269088d3315d74ade9668d55e7e78af29
bengris32 <bengris32@protonmail.ch>
2023-12-25 22:56:05 +0000

    pissarro: Drop redundant mtkpower blob fixup
    Change-Id: I259f64844bc34d8df4380a454f8e5613661de041
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- extract-files.sh
+++ extract-files.sh
@@ -58,9 +58,6 @@ function blob_fixup {
         vendor/lib*/hw/vendor.mediatek.hardware.pq@2.13-impl.so)
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
-	vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service)
-            "$PATCHELF" --replace-needed "android.hardware.power-V2-ndk_platform.so" "android.hardware.power-V2-ndk.so" "$2"
-            ;;
 	vendor/bin/hw/android.hardware.gnss-service.mediatek | \
         vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
             "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "$2"
