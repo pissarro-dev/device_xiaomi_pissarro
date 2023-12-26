b7be3834f6a4c96ec644d9f7444f531291d3a8a5
bengris32 <bengris32@protonmail.ch>
2023-12-26 21:05:38 +0000

    pissarro: Switch to common QTI vibrator service
    Change-Id: Ib58c8f5cd33ffbf0cf8c18ceb14c81cb21b1b2f9
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -373,11 +373,13 @@ PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek
 
 # Vibrator
+$(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
+
 PRODUCT_PACKAGES += \
-   vendor.qti.hardware.vibrator.service.pissarro
+    vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
