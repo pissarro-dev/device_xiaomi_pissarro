4938989007f76c6b71acbf045185754735a8085d
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-12-29 02:13:34 +0000

    pissarro: Move to health AIDL services
    Change-Id: Ibadf22d432791551c16d7f4e386b470574c390f8

--- device.mk
+++ device.mk
@@ -142,9 +142,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.mediatek \
+    android.hardware.health-service.mediatek-recovery
 
 # HIDL
 PRODUCT_PACKAGES += \
