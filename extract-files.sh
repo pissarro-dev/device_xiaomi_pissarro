aa73269ded824125cb1e0b065bd5b3efa6ba51b6
bengris32 <bengris32@protonmail.ch>
2023-12-31 00:40:38 +0000

    pissarro: Link Dolby service against V33 libstagefright_foundation
    * Surprise! It's the same goddamm deadlock.
    
    Change-Id: I68552624e15216963eae764cc897f5252fa1b4bf
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- extract-files.sh
+++ extract-files.sh
@@ -82,7 +82,8 @@ function blob_fixup {
 	vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc)
             sed -i 's/start/enable/' "$2"
             ;;
-	vendor/bin/hw/android.hardware.media.c2@1.2-mediatek)
+	vendor/bin/hw/android.hardware.media.c2@1.2-mediatek|\
+	vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service)
             "$PATCHELF" --add-needed "libstagefright_foundation-v33.so" "$2"
             ;;
     esac
