b10d7ebc3efcdf5c8c4fa589094695476a013641
rio004 <riyon336@gmail.com>
2024-01-02 17:16:33 +0000

    pissarro: Initial powerhint for MT6877
    Change-Id: I06f6303b8ae42fb46ad2da06d1ab3c20d945bd8b
    Signed-off-by: rio004 <riyon336@gmail.com>

--- device.mk
+++ device.mk
@@ -288,6 +288,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     vendor.mediatek.hardware.mtkpower@1.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Properties
 include $(LOCAL_PATH)/vendor_logtag.mk
 
