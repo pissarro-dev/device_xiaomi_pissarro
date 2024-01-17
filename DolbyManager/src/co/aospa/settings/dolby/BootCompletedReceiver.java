cbd3fed54aea56bab308c499f2df447d790d307c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-17 23:34:02 +0000

    [SQUASH] pissarro: Introduce DolbyManager
    * Implementation from https://github.com/AOSPA/android_device_realme_porsche/tree/uvite/DolbyManager
    
    Co-authored-by: Sugakesshaa <sugakesshaa@gmail.com>
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I2b6351a7e85b890b95c0ba485ad5ba67e40be998

---
+++ DolbyManager/src/co/aospa/settings/dolby/BootCompletedReceiver.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2023 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.settings.dolby;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "DolbyManager";
+    private static final boolean DEBUG = true;
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            return;
+        }
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        DolbyUtils.getInstance(context);
+    }
+}
--- device.mk
+++ device.mk
@@ -69,6 +69,7 @@ PRODUCT_PACKAGES += \
     libhapticgenerator
 
 PRODUCT_PACKAGES += \
+    DolbyManager \
     MtkInCallService
 
 PRODUCT_COPY_FILES += \
