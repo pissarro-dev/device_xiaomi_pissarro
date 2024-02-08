34c7dd8cdd2ab064f2439c55f9a18da48e4f7a78
Samrth Singh <63335952+samrth012@users.noreply.github.com>
2024-02-08 19:35:33 +0530

    Remove lineageos framework

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,8 +43,7 @@ TARGET_SCREEN_DENSITY := 440
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml 
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
