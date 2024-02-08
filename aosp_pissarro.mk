8979f5e15c757922245b6625aae9f0418d9e3e2a
Samrth Singh <63335952+samrth012@users.noreply.github.com>
2024-02-08 19:29:54 +0530

    Update and rename lineage_pissarro.mk to aosp_pissarro.mk

--- aosp_pissarro.mk
+++ aosp_pissarro.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/pissarro/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_pissarro
+PRODUCT_NAME := aosp_pissarro
 PRODUCT_DEVICE := pissarro
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
