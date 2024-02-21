242f16f820dbba576e6df2a1212e53bed6055b6c
Samrth Singh <63335952+samrth012@users.noreply.github.com>
2024-02-21 18:41:12 +0530

    Add dt2w

--- device.mk
+++ device.mk
@@ -118,6 +118,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.4.vendor
 
+# DT2W
+PRODUCT_PACKAGES += \
+    DT2W-Service-Pissarro 
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
